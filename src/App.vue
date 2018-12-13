06d2a495ef3dc3d20485ba107a9588bedf4993f2
Gaurav <gaurav.jestha@kelltontech.com>
2018-12-13 19:18:02 +0530

    my first vue app calculator

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <label>Enter first Number</label>
+    <input type="text"  @keyup="checking()" v-model="firstnumber"  required/>
+    <label>Enter second number</label>
+    <input type="text" @keyup="checkingsecondnumber()"v-model="secondnumber" required/>
+   <div class="calculator-result" >Result:{{this.result }}</div>
+   <app-child v-bind:firstnumber="firstnumber" v-bind:secondnumber="secondnumber" 
+   v-on:result="showresult($event)"></app-child>
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue'
+export default {
+  name: 'App',
+components:{
+'app-child':Child
+},
+data(){
+  return{
+    firstnumber:"",
+    secondnumber:"",
+    result:0
+}},
+methods:{
+showresult:function(show){
+  this.result=show
+},
+checking:function() {
+  var regexNumbers = /[^0-9-]*/g;                       
+  this.firstnumber=this.firstnumber.replace(regexNumbers,'');
+},
+checkingsecondnumber:function() {
+  var regexNumbers = /[^0-9-]*/g;                       
+  this.secondnumber=this.secondnumber.replace(regexNumbers,'');
+},
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+<button type="button" @click="add">Addition</button>
+<button type="button" @click="subtract">Subtract</button>
+<button type="button" @click="multiply">Multiply</button>
+<button type="button" @click="divide">Divide </button>
+</div>
+</template>
+<script>
+export default {
+props:["firstnumber","secondnumber"],
+data(){
+    return{
+        result:0,
+    }
+},
+methods:{
+add:function(){
+   this.$emit('result',parseInt(this.firstnumber) + parseInt(this.secondnumber))
+},
+subtract:function(){
+   this.$emit('result',parseInt(this.firstnumber) - parseInt(this.secondnumber))
+},
+multiply:function(){
+   this.$emit('result',parseInt(this.firstnumber) * parseInt(this.secondnumber))
+},
+divide:function(){
+   this.$emit('result',parseInt(this.firstnumber) / parseInt(this.secondnumber))
+}
+}
+}
+</script>
